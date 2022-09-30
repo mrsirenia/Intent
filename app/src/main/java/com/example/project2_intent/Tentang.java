8b46021179953f11096424d8c4f6ed7d90e786a3
mrsirenia <mauriesirenia@gmail.com>
2022-09-30 21:55:24 +0700

    Intent

---
+++ app/src/main/java/com/example/project2_intent/Tentang.java
@@ -0,0 +1,23 @@
+package com.example.project2_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Tentang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang);
+
+        Button back = findViewById(R.id.btnback);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
